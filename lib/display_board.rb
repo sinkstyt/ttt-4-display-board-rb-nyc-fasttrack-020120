7c3a76440aba92e7dd4486a7f73ec5cf05a9142f
Tyler Sinks <tyler.sinks@gmail.com>
2020-02-04 20:41:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,9 @@
 # out the current state.
 
 def display_board(board)
-   puts " #{baord[0]} | #{board[1]} | #{board[2]}"
+   puts " #{baord[0]} | #{board[1]} | #{board[2]} "
    puts "-----------"
-   puts ""
+   puts " #{baord[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{baord[6]} | #{board[7]} | #{board[8]} "
+end
